dd479956710fb8bea0063f45789c9027d7788e10
TouchBugs <2196692208@qq.com>
2024-05-31 22:36:44 +0800

    简化代码并增加学习率调整

--- CNN/原始网络结构bndp5.py
+++ CNN/原始网络结构bndp5.py
@@ -105,8 +105,8 @@ class GCN_MLP(nn.Module):
     def __init__(self):
         super(GCN_MLP, self).__init__()
         
-        self.fc2 = Linear(128, 64)
-        self.fc3 = Linear(64, 1)
+        self.fc2 = Linear(128, 256)
+        self.fc3 = Linear(256, 1)
         self.makeFeature = make_feature()
         self.SeLU = nn.SELU()
         # tanh激活函数
